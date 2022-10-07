6f43b3d9fbe58b06be777d22c4c4b7347a750731
LuanZdebski <luanmatheusbass@hotmail.com>
2022-10-07 13:23:55 -0300

    TelaLogin

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import TelaLogin from './TelaLogin';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => TelaLogin);
